6a0df076ad57726c8b6ccfdfe36bfd5ed580836f
sanjay <tecdevse@gmail.com>
2021-01-04 03:47:25 +0000

    Updated logic within Posts Controller to be more elegant.

--- cs348_Main/app/Http/Controllers/UserFilmProfileController.php
+++ cs348_Main/app/Http/Controllers/UserFilmProfileController.php
@@ -32,51 +32,37 @@ class UserFilmProfileController extends Controller
         $user = Auth::user();
         if ($user->can('update', UserFilmProfile::class)) {
 
+            $userFilmProfile = UserFilmProfile::where('user_id', Auth::user()->id)->first();
+            // Checks if user has a Film Profile
+
+            if($userFilmProfile == null)
+            {
+                $userFilmProfile = new UserFilmProfile();
+                $userFilmProfile->user_id = Auth::user()->id;
+            }
+            $userFilmProfile->favourite_film = $request['favFilm'];
+            $userFilmProfile->interests = $request['interests'];
+            $userFilmProfile->film_reasoning = $request['reasoning'];
+            $userFilmProfile->save();
+            $image = Image::where('imageable_type', 'App\Models\UserFilmProfile')->where('imageable_id', $userFilmProfile->id);
+
+            if ($image->exists()) {
+                //Removes old image from folder
+                unlink('storage/images/' . $userFilmProfile->image->image);
+                $image->delete();
+            }
+
             if ($request->hasFile('image_upload')) {
                 $fullFileName = $request->file('image_upload')->getClientOriginalName();
                 $filename = pathinfo($fullFileName, PATHINFO_FILENAME);
                 $fileExtension = $request->file('image_upload')->getClientOriginalExtension();
                 $fileNameToStore = $filename . '_' . time() . '.' . $fileExtension;
+
+                $newImage = new Image(['image' => $fileNameToStore]);
+                $userFilmProfile->image()->save($newImage);
+                $request->file('image_upload')->storeAs('public/images', $fileNameToStore);
             }
 
-            $userFilmProfile = UserFilmProfile::where('user_id', Auth::user()->id)->first();
-            // Checks if user has a Film Profile
-            if ($userFilmProfile != null) {
-                //Assigns new updated fields
-                $userFilmProfile->favourite_film = $request['favFilm'];
-                $userFilmProfile->interests = $request['interests'];
-                $userFilmProfile->film_reasoning = $request['reasoning'];
-
-                if ($request->hasFile('image_upload')) {
-                    //Removes Image Reference
-                    $image = Image::where('imageable_type', 'App\Models\UserFilmProfile')->where('imageable_id', $userFilmProfile->id);
-                    if ($image->exists()) {
-                        //Removes old image from folder
-                        unlink('storage/images/' . $userFilmProfile->image->image);
-                        $image->delete();
-                    }
-                    $newImage = new Image(['image' => $fileNameToStore]);
-                    $userFilmProfile->image()->save($newImage);
-                    $request->file('image_upload')->storeAs('public/images', $fileNameToStore);
-                }
-
-                $userFilmProfile->save();
-
-            } else {
-                //creates a new User Film Profile
-                $filmProfile = new UserFilmProfile();
-                $filmProfile->favourite_film = $request['favFilm'];
-                $filmProfile->interests = $request['interests'];
-                $filmProfile->film_reasoning = $request['reasoning'];
-                $filmProfile->user_id = Auth::user()->id;
-                $filmProfile->save();
-                if ($request->hasFile('image_upload')) {
-
-                    $image = new Image(['image' => $fileNameToStore]);
-                    $filmProfile->image()->save($image);
-                    $request->file('image_upload')->storeAs('public/images', $fileNameToStore);
-                }
-            }
             session()->flash('message', 'Profile is now up to date!');
             return redirect()->route('dashboard');
 
